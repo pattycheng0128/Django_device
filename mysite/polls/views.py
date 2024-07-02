b8ed3aa459021f35f096beaca0461429d3adc850
pattycheng <pattycheng@pattychengdeMacBook-Air.local>
2024-07-02 21:53:17 +0800

    新增註冊,登入,登出

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,11 @@ from .models import TB_1
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        name = request.POST.get('name', None)
+        age = request.POST.get('age', None)
+        obj = TB_1.objects.create(name=name, age=age)
+        obj.save()
+        
     user_data = TB_1.objects.get(name="Miya")
-    print(user_data)
     return render(request, 'polls/index.html', {'user_data': user_data})
